82ba84bc24a069c336c125c82f641e36d16f43c1
furkanevin <furkanevin00@gmail.com>
2023-03-16 20:38:00 +0300

    project const.

---
+++ main.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const videos = document.querySelectorAll('video');
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.play();
+      } else {
+        entry.target.pause();
+      }
+    });
+  });
+  videos.forEach((video) => {
+    observer.observe(video);
+  });
+});
